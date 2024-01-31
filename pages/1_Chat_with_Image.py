0eac65bc44adffd0e111276bfac993d680e80d61
Alavi Khan <leonasting@gmail.com>
2024-01-30 20:38:14 -0800

    Updated the Chat with Image to have API key input as prerequisite to use.

--- pages/1_Chat_with_Image.py
+++ pages/1_Chat_with_Image.py
@@ -19,74 +19,79 @@ def get_gemini_response(input,image):
     else:
        response = model.generate_content(image)
     return response.text
+def main ():
+    #st.set_page_config(page_title="Chatbot")
+    google_api_key = None
+    with st.sidebar:
+        google_api_key = st.text_input("Generative API Key", key="google_api_key", type="password")
+        "[Get an Generative AI API key](https://makersuite.google.com/app/apikey)"
+        "[View the source code]()"
+        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-with st.sidebar:
-    google_api_key = st.text_input("Generative API Key", key="google_api_key", type="password")
-    "[Get an Generative AI API key](https://makersuite.google.com/app/apikey)"
-    "[View the source code]()"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by Google Generative LLM")
-
-
-st.header("Chat with Image input and optional prompt")
-# Initialize session state for chat history if it doesn't exist
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-image=""
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)   
-submit=st.button("Tell me about the image")
-if submit:
-    if not google_api_key:
-        st.info("Please add your Generative AI API key to continue.")
-        st.stop()
-    elif uploaded_file is None:
-        st.info("Please add upload image to continue conversation.")
-        st.stop()
-    genai.configure(api_key=google_api_key)
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response=get_gemini_response("",image)
-    st.subheader("The Response is")
-    st.write(response)
-    st.session_state['chat_history'].append(("Bot", response))
-
-if prompt := st.chat_input():
-    if not google_api_key:
-        st.info("Please add your Generative AI API key to continue.")
-        st.stop()
-    else:
-        genai.configure(api_key=google_api_key)
-        ## Function to load OpenAI model and get respones
-        model = genai.GenerativeModel('gemini-pro-vision')
-        chat = model.start_chat(history=[])
+        if not google_api_key:
+            st.info("Please add your Generative AI API key to continue.")
         
-        if uploaded_file is None:
-            st.info("Please add upload image to continue conversation.")
             st.stop()
+        else:
+            genai.configure(api_key=google_api_key)
+        if google_api_key:
+            st.title("💬 Chatbot with image input")
+            st.caption("🚀 A streamlit chatbot powered by Google Generative LLM")
 
-        response=get_gemini_response(prompt,image)
-        # Add user query and response to session state chat history
-        
-        st.session_state['chat_history'].append(("You", prompt))
-        st.subheader("The Response is")
-        st.write(response)
-        # res=[]
-        # for chunk in response:
-        #     st.write(chunk.text)
-        #     res.append(chunk.text)
-        
-        st.session_state['chat_history'].append(("Bot", response))
-        
-if st.session_state['chat_history']:
-    st.subheader("The Chat History is")
-    
-    for role, text in st.session_state['chat_history']:
-        st.write(f"{role}: {text}")
-    
+
+            st.header("Chat with Image input and optional prompt")
+            # Initialize session state for chat history if it doesn't exist
+            if 'chat_history' not in st.session_state:
+                st.session_state['chat_history'] = []
+
+
+            uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+            image=""
+            if uploaded_file is not None:
+                image = Image.open(uploaded_file)
+                st.image(image, caption="Uploaded Image.", use_column_width=True)   
+            submit=st.button("Tell me about the image")
+            if submit:
+               
+                if uploaded_file is None:
+                    st.info("Please add upload image to continue conversation.")
+                    st.stop()
+                genai.configure(api_key=google_api_key)
+                model = genai.GenerativeModel('gemini-pro-vision')
+                response=get_gemini_response("",image)
+                st.subheader("The Response is")
+                st.write(response)
+                st.session_state['chat_history'].append(("Bot", response))
+
+            if prompt := st.chat_input():
+
+                    genai.configure(api_key=google_api_key)
+                    ## Function to load OpenAI model and get respones
+                    model = genai.GenerativeModel('gemini-pro-vision')
+                    chat = model.start_chat(history=[])
+                    
+                    if uploaded_file is None:
+                        st.info("Please add upload image to continue conversation.")
+                        st.stop()
+
+                    response=get_gemini_response(prompt,image)
+                    # Add user query and response to session state chat history
+                    
+                    st.session_state['chat_history'].append(("You", prompt))
+                    st.subheader("The Response is")
+                    st.write(response)
+                    # res=[]
+                    # for chunk in response:
+                    #     st.write(chunk.text)
+                    #     res.append(chunk.text)
+                    
+                    st.session_state['chat_history'].append(("Bot", response))
+                    
+            if st.session_state['chat_history']:
+                st.subheader("The Chat History is")
+                
+                for role, text in st.session_state['chat_history']:
+                    st.write(f"{role}: {text}")
+    if __name__ == "__main__":
+        main()    
 
